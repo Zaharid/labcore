1fc65b97888a2626f164ba7440e02f4080d21cff
zah <zaharid@gmail.com>
2013-11-16 23:20:45 +0100

    Added callable test

--- instruments/tests.py
+++ instruments/tests.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from instruments.models import Command
+from instruments.device_adapters import TestDevice
 from django.test import TestCase
 
 class CommandTestCase(TestCase):
@@ -19,7 +20,20 @@ class CommandTestCase(TestCase):
         c.save()
         
         self.assertEqual(c.parameter_set.get(name = 'p1').default_value, '5')
+        self.assertEqual(c.parameter_set.count(),3)
+    
+    
         
+    def test_callable_help(self):
+        c = Command.objects.get(name="c#test")
+        def ins():pass
+        ins.device = TestDevice()
+        f  =  c.make_callable(ins)
+        doclines =  f.__doc__.split('\n')
+        self.assertEqual(doclines[0], c.description)
+        self.assertEqual(doclines[2], c.command_string)
+    
     def tearDown(self):
+        print "Tear Down"
         Command.objects.get(name = "c#test").delete()
         
