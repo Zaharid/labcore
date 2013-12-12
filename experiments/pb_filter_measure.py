2076dafd27f4158ffdce7e70b662537c197355c7
Zahari Dimitrov <zaharid@gmail.com>
2013-12-12 19:41:46 +0100

    Started work in experiments

---
+++ experiments/pb_filter_measure.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 12 18:00:09 2013
+
+@author: zah
+"""
+import instruments
+def init():
+    osc.reset()
+    osc.set_probe(1,1)
+    osc.set_probe(2,1)
+    
