4a47651247acb5ddd4427fdc7920e44ddf4330d0
Zahari Dimitrov <zaharid@gmail.com>
2013-12-18 20:40:54 +0100

    Adaptative sampling seems to work but needs understanding.

--- sampler/adaptive_sampling.py
+++ sampler/adaptive_sampling.py
@@ -13,9 +13,9 @@ atan2 = np.arctan2
 pi = np.pi
 class AdaptiveSampler(object):
     def __init__(self, f, a ,b, sample_over = None, 
-                 min_angle =  0.07, init_points = 7, epsilon = 0, max_depth = 10, max_points = 300):
-        self.heapdict = PQDict()
-        self.data = {}
+                 min_angle =  0.07, init_points = 7, 
+                 epsilon = 0, max_points = 300):
+        
         self.f = f
         self.a = a
         self.b = b
@@ -23,7 +23,6 @@ class AdaptiveSampler(object):
         self.init_points = init_points
         self.min_angle = min_angle
         self.epsilon = epsilon
-        self.max_depth = max_depth
         self.max_points = max_points
         
         
@@ -34,13 +33,13 @@ class AdaptiveSampler(object):
          else:
             return yret
     
-    def get_prior(self, x, y ,xp, yp, xn, yn, *args):
+    def get_prior(self, x, y ,xp, yp, xn, yn):
         a1 = atan2(y-yp,x-xp + self.epsilon)
         a2 = atan2(yn-y,xn-x + self.epsilon)
         prior = -(min(np.abs(a1-a2), 2*pi-np.abs((a1-a2))))
         return prior
         
-    def eval_point(self, x, xp, yp, xn, yn, depth ,yret = None):
+    def eval_point(self, x, xp, yp, xn, yn ,yret = None):
         del self.data[x]
         if yret is None:    
             yret = self.f(x)
@@ -49,15 +48,15 @@ class AdaptiveSampler(object):
        
         prior = self.get_prior(x, y ,xp, yp, xn, yn)
         
-        if -prior > self.min_angle and depth <= self.max_depth: 
+        if -prior > self.min_angle: 
             #d1 = ((x-xp)/2. , xp, yp, x, y, depth+1)
             #d2 = ((xn-x)/2., x, y, xn, yn, depth +1)
             lx = (x+xp)/2.
             rx = (xn+x)/2.
             self.heapdict.additem(lx, prior)
             self.heapdict.additem(rx, prior)
-            self.data[lx] = [xp, yp, x, y, depth+1]
-            self.data[rx] =  [x, y, xn, yn, depth +1]
+            self.data[lx] = [xp, yp, x, y]
+            self.data[rx] =  [x, y, xn, yn]
             try:
                 xpd = self.data[xp]
             except KeyError:
@@ -82,6 +81,8 @@ class AdaptiveSampler(object):
         return x,yret
     
     def run(self):
+        self.heapdict = PQDict()
+        self.data = {}
         initx = np.linspace(self.a, self.b, self.init_points)
         initdelta = (initx[1] - initx[0])/2.
         inity = []
@@ -92,23 +93,31 @@ class AdaptiveSampler(object):
         for (i,xp) in enumerate(initx[0:-1]):
             d = [initx[i], self.get_y(inity[i]),
                  initx[i+1], self.get_y(inity[i+1]),
-                 0, ]
+                ]
             newx = xp + initdelta
-            self.data[newx] = d
+            
             prior = self.get_prior(newx, self.get_y(inity[i]), *d)
-            self.heapdict.additem(newx, prior)
+            if -prior > self.min_angle:
+                self.data[newx] = d
+                self.heapdict.additem(newx, prior)
                             
        
         n_points = self.init_points
+        print self.heapdict
         while n_points < self.max_points:
             try:
                 x , _ = self.heapdict.popitem()
+                print self.heapdict
             except KeyError:
                 return
             else:
                 d = self.data[x]
                 yield self.eval_point(x, *d)
                 n_points += 1
+                
+    def __call__(self):
+        return self.run()
+        
         
 def test():
     global s, it, x, y
@@ -116,9 +125,9 @@ def test():
     import matplotlib.pyplot as plt
     from matplotlib import animation
 
-    f = lambda x : stats.cauchy.pdf(x,0.5,.3)
+    f = lambda x : stats.cauchy.pdf(x,0.73,.3)
     fig = plt.figure()
-    ax = plt.axes(xlim=(-7, 7), ylim=(-0, 1.1))
+    ax = plt.axes(xlim=(-6, 6), ylim=(-0, 1.1))
     x = []
     y = []
     line, = ax.plot(x, y, 'o',lw=2)
@@ -126,7 +135,7 @@ def test():
     
     def init():
         global s, it, x, y
-        s = AdaptiveSampler(f, -7, 7, max_points = 300)
+        s = AdaptiveSampler(f, -4, 5.1, max_points = 50, epsilon = 1e-1)
         it = s.run()
         x = []
         y = []
@@ -140,6 +149,10 @@ def test():
         line.set_data(x,y)
         return line,
     anim = animation.FuncAnimation(fig, animate, init_func = init,
-                                   frames=80, interval=200, blit=True)
+                                   frames=80, interval=0, blit=True, 
+                                   )
     plt.show()
-test()
+    plt.plot(x,y, marker = 'o')
+    plt.show()
+
+#test()
