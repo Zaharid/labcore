3098caeca25fb453011b4d614fe031b2f11ba491
zah <zaharid@gmail.com>
2014-03-10 17:46:02 +0100

    Beginnig the upgrade of iobjects

--- labcore/iobjects/tests/base.py
+++ labcore/iobjects/tests/base.py
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Feb 28 14:12:46 2014
+Created on Sat Mar  8 21:59:43 2014
 
 @author: zah
 """
 
 import unittest
-import mongoengine as mg
-from mongoengine.connection import _get_db
-
-class MongoTest(unittest.TestCase):
+from labcore.mongotraits import documents
+from labcore.mongotraits.documents import  connect
+class BaseTest(unittest.TestCase):
     def setUp(self):
-        mg.connect('test')
-        db = _get_db()
-        db.connection.drop_database('test')
-        
+        connect('test')
+        documents.client.drop_database('test')
+
     def tearDown(self):
-        db = _get_db()
-        db.connection.drop_database('test')
-        mg.connection.disconnect()
+        documents.client.drop_database('test')
